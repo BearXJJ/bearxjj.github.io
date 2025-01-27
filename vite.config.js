90b2b3765b794da4214cbe22905934797eae1c75
BearXJJ <2329806036@qq.com>
2025-01-27 14:36:56 +0800

    change base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/bearxjj.github.io/',
+  // base: '/bearxjj.github.io/',
   plugins: [
     vue(),
     vueDevTools(),
